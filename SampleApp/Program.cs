05d8a04da7f78174087d844daabd4ff18fdd91c6
Austen <austensaffron@gmail.com>
2019-10-29 10:07:12 -0700

    added code that "iterates" through hardcoded config settings in the sample app.  Not sure if this is the intended idea?

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -4,29 +4,67 @@ using System.Collections.Generic;
 
 namespace SampleApp
 {
-    class Program 
+    class Program
     {
         static void Main()
         {
-            Console.WriteLine("Current Enviroment Config Values: ");
 
-            IConfig envConfig = new EnvironmentConfig();
+
+            IConfig environmentConfiger = new EnvironmentConfig();
 
             List<String> configSettings = new List<String>
             {
-                "USERPROFILE",
-                "PATH",
-                "TEMP",
-                "HOME"
+                "USERPROFILE=userprofile",
+                "TEMP=temp!"
             };
+            char[] splitOn = { '=' };
+
+            
+
+           
+            Console.WriteLine("Current Enviroment Config Values: ");
+
+            IterateThroughConfigValues(configSettings, splitOn, environmentConfiger);
+
+            
+            
+            
+            IConfig mockConfiger = new MockConfig();
+
+            Console.WriteLine("Setting MockConfig Values: ");
 
             foreach (string setting in configSettings)
             {
-                envConfig.GetConfigValue(setting, out string? value);
-                Console.WriteLine($"{setting}={value}");
+                string[] parsedEntry = setting.Split(splitOn, StringSplitOptions.RemoveEmptyEntries);
+                mockConfiger.SetConfigValue(parsedEntry[0], parsedEntry[1]);
+                Console.Write("mockconfig set with: ");
+                Console.Write($"{parsedEntry[0]}={parsedEntry[1]}");
+                Console.WriteLine();
+
+            }
+            Console.WriteLine();
+
+            IterateThroughConfigValues(configSettings, splitOn, mockConfiger);
+
+
+
+      
+
+        }
+
+        public static void IterateThroughConfigValues(List <string> configValues, char[] splitOnCharactersForConfigValues, IConfig iconfig)
+        {
+            Console.WriteLine($"Getting {iconfig.ToString()} Values: ");
+            foreach (string setting in configValues)
+            {
+                string[] parsedEntry = setting.Split(splitOnCharactersForConfigValues, StringSplitOptions.RemoveEmptyEntries);
+                iconfig.GetConfigValue(parsedEntry[0], out string? value);
+                Console.WriteLine($"{parsedEntry[0]}={value}");
                 Console.WriteLine();
 
             }
         }
     }
+
+
 }
