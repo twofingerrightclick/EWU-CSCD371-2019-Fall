186f469700ec4e582818409fb46af9c95d7c63ae
Austen <austensaffron@gmail.com>
2019-11-12 11:29:04 -0800

    removed dispose method, nothing unmanaged here.

--- Assignment6/GlobalSuppressions.cs
+++ Assignment6/GlobalSuppressions.cs
@@ -7,3 +7,6 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "<Pending>", Scope = "member", Target = "~M:Assignment6.Array`1.Contains(`0)~System.Boolean")]
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2208:Instantiate argument exceptions correctly", Justification = "<Pending>", Scope = "member", Target = "~M:Assignment6.Array`1.CopyTo(`0[],System.Int32)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "<Pending>", Scope = "member", Target = "~M:Assignment6.Array`1.#ctor(System.Int32)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1065:Do not raise exceptions in unexpected locations", Justification = "<Pending>", Scope = "member", Target = "~P:Assignment6.Array`1.IsReadOnly")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1065:Do not raise exceptions in unexpected locations", Justification = "<Pending>", Scope = "member", Target = "~P:Assignment6.Array`1.Item(System.Int32)")]
